8efd38a250509e04f88ccbdc3c8b32c85beab099
Manuel Da Silva <ktireds2803@gmail.com>
2023-02-13 20:43:42 +0100

    feat:[#6] login

--- app.js
+++ app.js
@@ -7,13 +7,10 @@ const passport = require("passport");
 const path = require ('path');
 
 // DB 
-
 require("./config/db.config");
 
 // HBS
-
 require("./config/hbs.config");
-
 
 const app = express();
 
@@ -25,32 +22,33 @@ app.use(express.static(path.join(__dirname + '../public')));
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 
+/* Session middlewares */
+const { sessionConfig } = require('./config/session.config');
+app.use(sessionConfig);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // static files
-
 app.use(express.static("public"));
 
 // routes
-
 const router = require("./config/routes.config");
 app.use("/", router)
 
 // Errors Middlewares
-
 app.use((req, res, next)=>{
   next(createError(404, "Resourse not found"));
 });
 
-// app.use((error, req, res, next)=>{
-//   console.log(error);
-//   let status = error.status || 500;
+app.use((error, req, res, next)=>{
+  console.log(error);
+  let status = error.status || 500;
 
-//   res.status(status).render("error",{
-//     message: error.message,
-//     error: req.app.get("env") === "development" ? error: {}
-//   })
-// })
-
+  res.status(status).render("error",{
+    message: error.message,
+    error: req.app.get("env") === "development" ? error: {}
+  })
+})
 
 const PORT = process.env.PORT || 3000
 
