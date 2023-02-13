c5b5ba2728683e0fb5dba8f1b5efc372faaa6e29
IvanLara-hub <116952338+IvanLara-hub@users.noreply.github.com>
2023-02-13 20:46:44 +0100

    wip

---
+++ seeds/seeds.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Product = require("../models/Product.model");
+
+require("../db.config");
+
+const products = require("../poducts.json");
+
+mongoose.connection.once("open", () => {
+  console.info(
+    `*** Connected to the database ${mongoose.connection.db.databaseName} ***`
+  );
+
+  mongoose.connection.db
+    .dropDatabase()
+    .then(() => {
+      console.info("Db.config has been cleared");
+
+      return Product.create(products);
+    })
+    .then((createdProducts) => {
+      createdProducts.forEach((product) => {
+        console.log(`Product with name ${product.name} has been created`);
+      });
+      console.log(
+        `A total of ${createdProducts.length} drones has been created`
+      );
+    })
+    .catch((err) => console.error(err))
+    .finally(() => {
+      mongoose.connection.close(function () {
+        console.log("Mongoose disconnected");
+        process.exit(0);
+      });
+    });
+});
