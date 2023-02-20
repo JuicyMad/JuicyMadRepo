71a625acd07bae9abf9ed649c91c7639a9360d51
MartaFR2801 <martafrancoramos28@gmail.com>
2023-02-20 21:39:35 +0100

    feat:[#20] cambio de idioma,autenticado de productos

--- config/hbs.config.js
+++ config/hbs.config.js
@@ -6,6 +6,11 @@ hbs.registerPartials(path.join(__dirname, '../views/partials'));
 hbs.registerHelper('isAdmin', function (options) {
   const { currentUser } = options.hash;
 
+  if (!currentUser) {
+    console.log('NOT LOGGED IN!');
+    return;
+  }
+
   if (currentUser.role === "Admin") {
     return options.fn(this);
   } else {
--- config/routes.config.js
+++ config/routes.config.js
@@ -22,17 +22,11 @@ router.get("/", (req, res, next) => res.send("JUICY WORLD"));
 
 // auth
 router.get("/signup", authMiddleware.isNotAuthenticated, authController.signup);
-router.post(
-  "/signup",
-  authMiddleware.isNotAuthenticated,
-  authController.doSignup
+router.post("/signup",authMiddleware.isNotAuthenticated, authController.doSignup
 );
 
 router.get("/login", authMiddleware.isNotAuthenticated, authController.login);
-router.post(
-  "/login",
-  authMiddleware.isNotAuthenticated,
-  authController.doLogin
+router.post("/login",authMiddleware.isNotAuthenticated,authController.doLogin
 );
 
 router.get("/logout", authMiddleware.isAuthenticated, authController.doLogout);
