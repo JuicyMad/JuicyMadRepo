c4d59b0dbba1b86592ab32efa1f4e6a7bf4ecee5
IvanLara-hub <116952338+IvanLara-hub@users.noreply.github.com>
2023-02-22 19:14:52 +0100

    remove middelware of home

--- config/routes.config.js
+++ config/routes.config.js
@@ -10,24 +10,25 @@ const cartController = require("../controllers/cart.controller");
 const authMiddleware = require("../middlewares/auth.middlewares");
 const adminMiddleware = require("../middlewares/admin.middleware");
 
-
 const GOOGLE_SCOPES = [
-    "https://www.googleapis.com/auth/userinfo.profile",
-    "https://www.googleapis.com/auth/userinfo.email"
-  ]
-  
+  "https://www.googleapis.com/auth/userinfo.profile",
+  "https://www.googleapis.com/auth/userinfo.email",
+];
 
 /* Main route */
 router.get("/", (req, res, next) => res.send("JUICY WORLD"));
 
 // auth
 router.get("/signup", authMiddleware.isNotAuthenticated, authController.signup);
-router.post("/signup",authMiddleware.isNotAuthenticated, authController.doSignup
+router.post(
+  "/signup",
+  authMiddleware.isNotAuthenticated,
+  authController.doSignup
 );
 
 router.get("/login", authMiddleware.isNotAuthenticated, authController.login);
 router.post(
- "/login",
+  "/login",
   authMiddleware.isNotAuthenticated,
   authController.doLogin
 );
@@ -36,28 +37,57 @@ router.get("/logout", authMiddleware.isAuthenticated, authController.doLogout);
 
 // user
 
-router.get("/home", authMiddleware.isAuthenticated, userController.home);
+router.get("/home", userController.home);
 router.get("/profile", authMiddleware.isAuthenticated, userController.profile);
 
 //Sign with google
-router.get('/login/google',passport.authenticate('google-auth', { scope: GOOGLE_SCOPES }))
-router.get('/auth/google/callback', authController.doLoginGoogle)
+router.get(
+  "/login/google",
+  passport.authenticate("google-auth", { scope: GOOGLE_SCOPES })
+);
+router.get("/auth/google/callback", authController.doLoginGoogle);
 
 // products
 
-
 router.get("/products", productController.list);
-router.get("/products/create", authMiddleware.isAuthenticated, adminMiddleware.isAdmin, productController.create);
-router.post("/products/create",authMiddleware.isAuthenticated, adminMiddleware.isAdmin, productController.doCreate);
-router.get("/products/:id/edit", authMiddleware.isAuthenticated, adminMiddleware.isAdmin,productController.update);
-router.post("/products/:id/edit",authMiddleware.isAuthenticated, adminMiddleware.isAdmin, productController.doUpdate);
-router.post("/products/:id/delete", authMiddleware.isAuthenticated, adminMiddleware.isAdmin,productController.delete);
-router.get("/products/:slug",productController.detail);
-
-
+router.get(
+  "/products/create",
+  authMiddleware.isAuthenticated,
+  adminMiddleware.isAdmin,
+  productController.create
+);
+router.post(
+  "/products/create",
+  authMiddleware.isAuthenticated,
+  adminMiddleware.isAdmin,
+  productController.doCreate
+);
+router.get(
+  "/products/:id/edit",
+  authMiddleware.isAuthenticated,
+  adminMiddleware.isAdmin,
+  productController.update
+);
+router.post(
+  "/products/:id/edit",
+  authMiddleware.isAuthenticated,
+  adminMiddleware.isAdmin,
+  productController.doUpdate
+);
+router.post(
+  "/products/:id/delete",
+  authMiddleware.isAuthenticated,
+  adminMiddleware.isAdmin,
+  productController.delete
+);
+router.get("/products/:slug", productController.detail);
 
 // cart
-router.get("/cart", cartController.cart)
-router.post("/:id/addToCart", authMiddleware.isAuthenticated, cartController.editCart)
+router.get("/cart", cartController.cart);
+router.post(
+  "/:id/addToCart",
+  authMiddleware.isAuthenticated,
+  cartController.editCart
+);
 
 module.exports = router;
